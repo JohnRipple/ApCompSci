217c99f14399cc2276bfd39d1a0bd46de7c56ed5
JohnRipple <ripplejrhi@gmail.com>
2018-11-09 13:19:36 -0700

    f

--- syntax_ouptput/Car/src/Car.java
+++ syntax_ouptput/Car/src/Car.java
@@ -1,42 +1,36 @@
-
 public class Car {
-	private int[][] myCars;
 	private String make;
 	private String model;
 	private String color;
-	
-	public Car() {
-		
+
+	public Car(String mk, String mdl, String c) {
+		make = mk;
+		model = mdl;
+		color = c;
 	}
-	public Car(String ma, String mo, String co) {
-		make = ma;
-		model = mo;
-		color = co;
-	}
-	
-	public int[][] getMyCars() {
-		return myCars;
-	}
-	public void setMyCars(int[][] myCars) {
-		this.myCars = myCars;
-	}
-	public String getModel() {
-		return model;
-	}
-	public void setModel(String model) {
-		this.model = model;
-	}
-	public String getColor() {
-		return color;
-	}
-	public void setColor(String color) {
-		this.color = color;
-	}
+
 	public String getMake() {
 		return make;
 	}
-	public void setMake(String make) {
-		this.make = make;
+
+	public void setMake(String mk) {
+		make = mk;
 	}
-	
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String md) {
+		model = md;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String clr) {
+		color = clr;
+	}
+
 }
--- syntax_ouptput/Car/src/Car2dBuilder.java
+++ syntax_ouptput/Car/src/Car2dBuilder.java
@@ -1,59 +1,29 @@
 
 public class Car2dBuilder {
 	public static void main(String[] args) {
-		int rows = 30;
-		int columns = 5;
+		String[] make = {"Toyota","Chevy","Ford"};
+		String[] model = {"Rav4","Corolla","Tacoma","Malibu","Blazer","Avalanche","Focus","Tempo","Explorer"};
+		String[] color = {"Blue", "Red", "Black"};
+		int mo = 0, max1 = 0, min1 = 0;
+		int rows = 5;
+		int columns = 2;
 		Car[][] myCars = new Car[rows][columns];
 		
-		String[] make = new String[3];
-		make[0] = "Toyota";
-		make[1] = "Chevy";
-		make[2] = "Ford";
-		
-		String[] model = new String[9];
-		model[0] = "Rav4";
-		model[1] = "Corolla";
-		model[2] = "Tacoma";
-		model[3] = "Malibu";
-		model[4] = "Blazer";
-		model[5] = "Avalanche";
-		model[6] = "Focus";
-		model[7] = "Tempo";
-		model[8] = "Explorer";
-		
-		String[] color = {"Blue", "Red", "Purple"};
-		
-		int max = 3;
-		int min = 0;
-		int mo;
-
 		for(int i = 0; i < rows; i++) {
 			for(int j = 0; j < columns; j++) {
-				int ma = (int)(Math.random() * (max - min) + min);
-				int max1;
-				int min1;
-				int co = (int)(Math.random() * (max - min) + min);
-				
-				if(ma == 0) {
-					max1 = 3;
-					min1 = 0;
-					mo = (int)(Math.random() * (max1 - min1) + min1);
-					myCars[i][j] = new Car(make[ma], model[mo], color[co]);
-				} else if (ma == 1) {
-					max1 = 3;
-					min1 = 6;
-					mo = (int)(Math.random() * (max1 - min1) + min1);
-					myCars[i][j] = new Car(make[ma], model[mo], color[co]);
-				} else if (ma == 2) {
-					max1 = 6;
-					min1 = 9;
-					mo = (int)(Math.random() * (max1 - min1) + min1);
-					myCars[i][j] = new Car(make[ma], model[mo], color[co]);
+				int rand = (int)(Math.random() * (3 - 0) + 0);
+				int co = (int)(Math.random() * (3 - 0) + 0);
+				if(rand == 0) {
+					max1 = 3; min1 = 0;
+				} else if (rand == 1) {
+					max1 = 6; min1 = 3;
+				} else if (rand == 2) {
+					max1 = 9; min1 = 6;
 				}
+				mo = (int)(Math.random() * (max1 - min1) + min1);
+				myCars[i][j] = new Car(make[rand], model[mo], color[co]);
 				System.out.println("Location [" + i + "]" + "[" + j + "] = " + myCars[i][j].getMake() + " " + myCars[i][j].getModel() + " " + myCars[i][j].getColor());
-				
 			}
 		}
-		
 	}
 }
