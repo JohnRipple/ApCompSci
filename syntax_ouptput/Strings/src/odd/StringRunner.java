b50efa5e2d4c12a55ed774faff40a4fa961f9bde
JohnRipple <ripplejrhi@gmail.com>
2019-01-17 11:01:23 -0700

    Fixed one of them

--- syntax_ouptput/Strings/src/odd/StringRunner.java
+++ syntax_ouptput/Strings/src/odd/StringRunner.java
@@ -15,7 +15,15 @@ public class StringRunner
 {
 	public static void main ( String[] args )
 	{
-		StringOddOrEven s = new StringOddOrEven("ca");
-		System.out.println(s.isEven());
+		int t = 1 ;
+		while(t == 1) {
+			Scanner s = new Scanner(System.in);
+			System.out.println("Enter a word :: ");
+			StringOddOrEven b = new StringOddOrEven(s.nextLine());
+			System.out.println(b.isEven());
+			if (s.nextLine().equals("")) {
+				t = 0;
+			}
+		}
 	}
 }
--- syntax_ouptput/Strings/src/repeats/First.java
+++ syntax_ouptput/Strings/src/repeats/First.java
@@ -13,7 +13,7 @@ public class First
 	 String check = a.substring(0,1);
 	   	if(a.contains(check)) {
 	   		if(a.indexOf(check) + 1 < a.length()-1) {
-	   			s = a.substring(a.indexOf(check) + 1, a.length()-1);
+	   			s = a.substring(a.indexOf(check) + 1);
 	   		} else {
 	   			return "no";
 	   		}
