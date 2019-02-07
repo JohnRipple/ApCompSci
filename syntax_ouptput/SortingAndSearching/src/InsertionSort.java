91d59369e13aca7f1202ac1a9b0dd9fbb6a4a37d
JohnRipple <ripplejrhi@gmail.com>
2019-02-07 11:54:38 -0700

    idk

--- syntax_ouptput/SortingAndSearching/src/InsertionSort.java
+++ syntax_ouptput/SortingAndSearching/src/InsertionSort.java
@@ -3,6 +3,9 @@ public class InsertionSort {
 	public static void main(String[] args) {
 		int[] r = {9,2,8,5,1};
 		insertionSort(r);
+		for(int i = 0; i < r.length; i++){
+			System.out.println(r[i]);
+		}
 	}
 
 	static void insertionSort( int[] stuff)
---
+++ syntax_ouptput/SortingAndSearching/src/Sorting.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class Sorting {
+	static int[] nums = { 9, 2, 8, 5, 1 };
+
+	public static void main(String[] args) {
+		System.out.println("Start of array passing: " + Arrays.toString(nums));
+		//selectionSort(nums);
+		insertionSort(nums);
+		System.out.println("End of array passing: " + Arrays.toString(nums));
+	}
+
+	public static void selectionSort(int[] ray) {
+		for (int i = 0; i < ray.length - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < ray.length; j++) {
+				if (ray[j] < ray[min])
+					min = j; // find location of smallest
+			}
+			if (min != i) {
+				int temp = ray[min];
+				ray[min] = ray[i];
+				ray[i] = temp; // put smallest in spot i
+			}
+		}
+	}
+
+	public static void insertionSort(int[] stuff) {
+		for (int i = 1; i < stuff.length; ++i) {
+			int val = stuff[i];
+			int j = i;
+			while (j > 0 && val < stuff[j - 1]) {
+				stuff[j] = stuff[j - 1];
+				j--;
+			}
+			stuff[j] = val;
+		}
+	}
+
+}
