abe56689a95e73ed01ade0ca1afdc16d8bc47731
JohnRipple <ripplejrhi@gmail.com>
2019-03-25 17:47:13 -0600

    battleship

--- syntax_ouptput/Battleship/src/Battleship.java
+++ syntax_ouptput/Battleship/src/Battleship.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Battleship {
     private Player player1;
     private Player player2;
+    private int score1 = 0; private int score2 = 0;
     public static void main(String[] args) {
         Battleship b = new Battleship();
         b.setupGame();
@@ -21,18 +22,79 @@ public class Battleship {
         player1 = new Player(scan.nextLine());
         player1.setupShips();
         System.out.println("Enter Player 2 Name: ");
-        player2 = new Player(scan.nextLine());
+       // player2 = new Player(scan.nextLine());
+        //player2.setupShips();
     }
 
     public void playGame(){
-
+        int turn = 0;
+        //while(checkForWinner() == false){
+            if(turn == 0){
+                boards(turn);
+                turn = 1;
+            } else {
+                boards(turn);
+                turn = 0;
+            }
+       // }
     }
 
-    private void checkForWinner(){
-
+    private boolean checkForWinner(){
+        if (score1 == 9){
+            return true;
+        } else if (score2 == 9){
+            return true;
+        }
+        return false;
     }
 
     private void displayOpponentGameBoardRealTime(){
+
+    }
+
+    private void boards(int f) {
+        boolean[][] o;
+        boolean[][] s;
+        if (f == 0) {
+            o = player1.getOpponentGameBoard();
+            s = player1.getSelfGameBoard();
+        } else {
+            o = player2.getOpponentGameBoard();
+            s = player2.getSelfGameBoard();
+        }
+            System.out.print("  ");
+            for(int i = 0; i < 10; i++){
+                System.out.print(i + "  ");
+            }
+            System.out.println();
+            for(int i = 0; i < 10; i++){
+                System.out.print(i + " ");
+                for(int j = 0; j < 10; j++){
+                    if(s[i][j] == false) {
+                        System.out.print("-  ");
+                    } else {
+                        System.out.print("T  ");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+        System.out.print("  ");
+        for(int i = 0; i < 10; i++){
+            System.out.print(i + "  ");
+        }
+        System.out.println();
+            for(int i = 0; i < 10; i++){
+                System.out.print(i + " ");
+                for(int j = 0; j < 10; j++){
+                    if(o[i][j] == false) {
+                        System.out.print("-  ");
+                    } else {
+                        System.out.print("T  ");
+                    }
+                }
+                System.out.println();
+            }
 
     }
 
--- syntax_ouptput/Battleship/src/Player.java
+++ syntax_ouptput/Battleship/src/Player.java
@@ -12,7 +12,36 @@ public class Player {
     }
 
     public void setupShips() {
+        System.out.println("Use 0-9 for the first location and - then 0-9 again.");
         Scanner sc = new Scanner(System.in);
+        String[] bat;
+        boolean repeat = false;
+        while (repeat == false){
+            System.out.println("Battleship Location 4 spots: ");
+            bat = sc.nextLine().split("\\s|-");
+            repeat = check(bat);
+            if(bat.length != 8)
+                repeat = false;
+            setScoreBoard(bat);
+        }
+        repeat = false;
+        while(repeat == false){
+            System.out.println("Cruiser Location 3 spots: ");
+            String[] cru = sc.nextLine().split("\\s|-");
+            repeat = check(cru);
+            if(cru.length != 6)
+                repeat = false;
+            setScoreBoard(cru);
+        }
+        repeat = false;
+        while (repeat == false){
+            System.out.println("Submarine Location 2 spots: ");
+            String[] sub = sc.nextLine().split("\\s|-");
+            repeat = check(sub);
+            if(sub.length != 4)
+                repeat = false;
+            setScoreBoard(sub);
+        }
 
     }
 
@@ -26,8 +55,72 @@ public class Player {
     public boolean[][] getOpponentGameBoard(){
         return opponentGameBoard;
     }
+
+    public boolean[][] getSelfGameBoard(){
+        return selfGameBoard;
+    }
+
     public boolean won() {
         return false;
+    }
+
+    private boolean check(String[] len){
+        if (len.length < 4){
+            return false;
+        }
+        int count = 0;
+        int[] x = new int[len.length/2];
+        int[] y = new int[x.length];
+        for(int i = 0; i < len.length; i+=2){
+            x[count] = Integer.parseInt(len[i]);
+            y[count] = Integer.parseInt(len[i+1]);
+            count++;
+        }
+        count = 0;
+        int next = 0;
+        boolean horizontal = false;
+        boolean vertical = false;
+        for(int i = 0; i < x.length-1; i++){
+            if(x[i] == x[i+1] - 1 || x[i] == x[i+1]-1) {
+                next++;
+            }
+        }
+        if (next == x.length-1){
+            vertical = true;
+        }
+        if(vertical == false) {
+            next = 0;
+                for(int i = 0; i < x.length-1; i++) {
+                    if (x[i] == x[i + 1]) {
+                        next++;
+                    }
+                }
+                if (next == x.length-1)
+                    horizontal = true;
+        }
+        if(horizontal == true){
+            for(int i = 0; i < y.length-1; i++){
+                if (y[i] == y[i+1] - 1 || y[i] == y[i+1]-1) {
+                    count++;
+                }
+            }
+        } else if (vertical == true){
+            for(int i = 0; i < y.length-1; i++){
+                if (y[i] == y[i+1]) {
+                    count++;
+                }
+            }
+        }
+        if (count == len.length/2-1){
+            return true;
+        }
+        return false;
+    }
+
+    private void setScoreBoard(String[] s){
+        for(int i = 0; i < s.length; i+=2){
+            selfGameBoard[Integer.parseInt(s[i])][Integer.parseInt(s[i+1])] = true;
+        }
     }
 
 }
