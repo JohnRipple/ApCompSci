222d636194559622da2c70177e00b1895e8013bf
JohnRipple <ripplejrhi@gmail.com>
2019-04-08 16:20:34 -0600

    import sys

---
+++ Code Quest/MinesCoding/mines18.edays.py
@@ -0,0 +1,5 @@
+import sys
+li=[]
+for i in sys.stdin:
+    li.append(int(i))
+print(li[0]*2)
---
+++ Code Quest/MinesCoding/mines18.sequence.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.stdin = open('mines18.sequence.txt', 'r')
+
+li=[]
+for i in sys.stdin:
+    li.append(i)
