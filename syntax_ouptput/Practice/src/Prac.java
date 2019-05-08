f39d99b5edfdbbdfd7fe7bb7712b014726285e67
JohnRipple <ripplejrhi@gmail.com>
2019-05-08 14:40:36 -0600

    ArrayList Practice

--- syntax_ouptput/Practice/src/Prac.java
+++ syntax_ouptput/Practice/src/Prac.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Prac {
     ArrayList<String> myList = new ArrayList<>();
@@ -27,6 +28,11 @@ public class Prac {
         p.myList.add("12");
         p.myList.add("124");
 
+        List<Integer> l = new ArrayList<Integer>();
+        l.add(5);
+        l.add(Integer.valueOf((int)2.6));
+        double d = (double)5/6;
+        System.out.println(d);
 
         p.removeWordsOfLength(1);
         System.out.println(p.myList);
